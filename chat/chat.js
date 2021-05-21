6da0b3801bfce7de52435174802319638477867f
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-05-20 21:03:48 -0500

    class work

--- chat/chat.js
+++ chat/chat.js
@@ -34,16 +34,23 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector(`.message-form`).classList.remove(`hidden`)
 
     // 🔥 Step 4 begins here
+
     // get a reference to the "submit message" button
+    let submitButton = document.querySelector(`#submit-message`)
 
     // handle the "submit message" button being clicked
+    submitButton.addEventListener(`click`, async function(event) {
 
       // ignore the default form submit behavior
+      event.preventDefault()
 
       // get a reference to the "message" input box
+      let messageField = document.querySelector(`#message`)
 
       // get the message that was typed in the "message" input box
+      let message = messageField.value
 
+      console.log(message)
       // the lambda function to create a message is already written and available at:
       // https://kiei451.com/.netlify/functions/create_message?userName=_______&body=________
       // it accepts two querystring parameters: 
@@ -52,27 +59,42 @@ firebase.auth().onAuthStateChanged(async function(user) {
       // if successfully called, the return value of the lambda is an Array of all messages
 
       // build the URL of the lambda function
+      let url = `https://kiei451.com/.netlify/functions/create_message?userName=${user.displayName}&body=${message}`
 
       // Fetch the url, wait for a response, store the response in memory
+      let response = await fetch(url)
 
       // Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
 
       // Grab a reference to the element with class name "messages" in memory
+      let messageElement = document.querySelector(`.messages`)
 
       // Clear anything already in the "messages" element
+      messageElement.innerHTML = ``
 
       // Loop through the JSON data, for each Object representing a message:
+      for (let i = 0; i < json.length; i++) {
 
         // Store each object ("message") in memory
+        let messageObject = json[i]
 
+        console.log(messageObject)
         // Create some markup using the post data, insert into the "messages" element
+        messageElement.insertAdjacentHTML(`beforeend`, `
+        <div class="md:mx-0 mx-4 mt-8 w-1/2 mx-auto">
+          <span class="font-bold">${messageObject.userName} says... </span>
+          ${messageObject.body}
+        </div>
+        `)
         // Sample HTML to use:
-        // <div class="md:mx-0 mx-4 mt-8 w-1/2 mx-auto">
-        //   <span class="font-bold">Brian says... </span>
-        //   I love tacos!
-        // </div>
+
+      }
 
       // clear out the messages input box
+      messageField.value = ``
+
+    })
   } else {
     // user is not logged-in, so show login
     // Initializes FirebaseUI Auth
