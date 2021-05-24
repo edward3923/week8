dde82d148b6d920273c04debd91af97f2ce11936
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-05-24 18:43:47 -0500

    committing

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,12 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
+  apiKey: "AIzaSyAbFbDKkjy18OJKMpnNFRSemFBnOHgqvhI",
+  authDomain: "kiei-451-3a150.firebaseapp.com",
+  projectId: "kiei-451-3a150",
+  storageBucket: "kiei-451-3a150.appspot.com",
+  messagingSenderId: "136717414465",
+  appId: "1:136717414465:web:e6a733a02b4b3145c8d6c0"
 } // replace
 
 if (!firebase.apps.length) {
